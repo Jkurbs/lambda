3481aba448cd151c2a7fb29dec28a88e4fd0ab5f
Kerby <kerby.jean@hotmail.fr>
2020-01-24 12:02:39 -0500

    Setup initial ViewController

--- ios-sprint-challenge-movie-list/Movie List/AppDelegate.swift
+++ ios-sprint-challenge-movie-list/Movie List/AppDelegate.swift
@@ -5,6 +5,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+         /// Setup initial ViewController
+         let initialViewController = MovieListVC()
+         let navigationController = UINavigationController(rootViewController: initialViewController)
+         self.window?.rootViewController = navigationController
+         self.window?.makeKeyAndVisible()
+        
         return true
     }
 }
