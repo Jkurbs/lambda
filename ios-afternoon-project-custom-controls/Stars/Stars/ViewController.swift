3b7346b155528afbc392febfed281df062ad942c
Kerby Jean <kurbs@Kerbys-MacBook-Pro.local>
2020-02-20 15:24:39 -0500

    First commit

---
+++ ios-afternoon-project-custom-controls/Stars/Stars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stars
+//
+//  Created by Kerby Jean on 2/20/20.
+//  Copyright © 2020 Kerby Jean. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
